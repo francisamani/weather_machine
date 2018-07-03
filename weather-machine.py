e4bba31c162e442069c0326b327bbbf7d5b5f224
Francis Amani <37621187+francisamani@users.noreply.github.com>
2018-07-03 22:11:25 +0300

    Add files via upload

---
+++ weather-machine.py
@@ -0,0 +1,12 @@
+# Francis Oludhe
+# Weather Machine
+
+import random
+import sys
+import time
+
+weather = ['Sunny', 'Rainy', 'Snowy', 'Windy', 'Sunny & Windy']
+
+today = (random.choice(weather))
+
+print ("Today's weather will be", today+'.')
